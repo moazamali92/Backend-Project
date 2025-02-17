81b64b872f93073d576356eee7f3cbaf750e6ed1
MOAZAM ALI <moazamali6561@gmail.com>
2025-02-18 00:23:13 +0500

    add user and video

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,14 +1,41 @@
-// const asyncHandler= ()=>{}
+const asyncHandler = (requestHandler) => {
+    (req, res ,next) =>{
+        Promise.resolve().catch((error) => next(error))
+    }
+
+}
+
+
+
+
+
 
 export  {asyncHandler}
 
-const asyncHandler = (fn) => async (req, res , next) => {
-    try {
-        
-    } catch (error) {
-        res.status(err.code || 500).json({
-            success: false,
-            message:err.message
-        })
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const asyncHandler = (fn) => async (req, res , next) => {
+//     try {
+//         await fn(req  ,res ,next )
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message:err.message
+//         })
+//     }
+// }
