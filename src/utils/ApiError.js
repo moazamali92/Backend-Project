433b2c3fce6597d8a27568261ebf3a95efe6ace3
MOAZAM ALI <moazamali6561@gmail.com>
2025-02-18 23:34:49 +0500

    saved file on cloudinary and do multer

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
         statuscode,
         message="something went wrong",
         errors=[],
-        statck=""
+        stack=""
     ){
         super(message)
         this.statuscode= statuscode
@@ -13,8 +13,8 @@ class ApiError extends Error{
         this.errors = errors
 
 
-        if (statck) {
-            this.statck=statck
+        if (stack) {
+            this.stack=stack
             
         } else {
             Error.captureStackTrace(this ,this.constructor)
